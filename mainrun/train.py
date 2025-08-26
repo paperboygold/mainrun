207692bcd4d0e18129766d126c348bb156368d97
Lucas 'Paperboy' Rose-Winters <lucas@sanguinehost.com>
2025-08-26 14:40:15 +0000

    Mainrun auto checkpoint

--- mainrun/train.py
+++ mainrun/train.py
@@ -22,7 +22,7 @@ class Hyperparameters:
     n_head: int = 8
     d_model: int = 512
     dropout: float = 0.1
-    lr: float = 6e-4
+    lr: float = 8e-4
     weight_decay: float = 0.1
     evals_per_epoch: int = 3
     
